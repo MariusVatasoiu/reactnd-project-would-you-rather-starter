339fab18af32aa1fbdd7fd7cc45149a1bd8dcac1
Marius Vatasoiu <marius.vatasoiu@outlook.com>
2021-07-14 03:13:13 +0300

    feat: add percentages on results page

--- src/components/QuestionResults.js
+++ src/components/QuestionResults.js
@@ -7,8 +7,9 @@ class QuestionResults extends Component {
     const { author, authedUser, question } = this.props;
     const oneVotes = question.optionOne.votes.length;
     const twoVotes = question.optionTwo.votes.length;
-    const totalVotes = oneVotes +
-      twoVotes;
+    const totalVotes = oneVotes + twoVotes;
+    const onePercentage = (oneVotes / totalVotes) * 100;
+    const twoPercentage = (twoVotes / totalVotes) * 100;
     return (
       <div className="page">
         <h3>Asked by {author.name}</h3>
@@ -25,7 +26,9 @@ class QuestionResults extends Component {
           </p>
           <p>
             <small>
-              <strong>{oneVotes} out of {totalVotes} votes</strong>
+              <strong>
+                {oneVotes} out of {totalVotes} votes ({onePercentage}%)
+              </strong>
             </small>
           </p>
         </section>
@@ -38,7 +41,9 @@ class QuestionResults extends Component {
           </p>
           <p>
             <small>
-              <strong>{twoVotes} out of {totalVotes} votes</strong>
+              <strong>
+                {twoVotes} out of {totalVotes} votes ({twoPercentage}%)
+              </strong>
             </small>
           </p>
         </section>
