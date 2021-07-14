a2b2f2336fe6d0f1ba666a50cfbca8c15a4358f4
Marius Vatasoiu <marius.vatasoiu@outlook.com>
2021-07-14 03:09:42 +0300

    feat: update new question page

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -2,6 +2,16 @@ function NewQuestion() {
   return (
     <div className="container">
       <h1>New Question</h1>
+      <h3>Create new question</h3>
+      <p>Complete the question:</p>
+      <h4>Would you rather ...</h4>
+      <input placeholder="Enter Option One Text Here" />
+      <p>OR</p>
+      <input placeholder="Enter Option Two Text Here" />
+
+      <div>
+        <button>Submit</button>
+      </div>
     </div>
   );
 }
